c63d8e6ba389affe64e25952d7650cdc99cf2344
Clément Menou <clementmenou0@gmail.com>
2023-10-06 23:44:17 +0200

    Babylon : ortho cam begining

--- main.js
+++ main.js
@@ -8,27 +8,38 @@ const createScene = async function() {
   const scene = new BABYLON.Scene(engine);
 
   // scene.createDefaultCameraOrLight(true, false, true);
-  scene.createDefaultLight();
+
+  const camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene)
   
-  const camera = new BABYLON.ArcRotateCamera('camera', 2, 1, 10, new BABYLON.Vector3(0, 0, 0), scene)
-  camera.attachControl(true);
-  camera.setPosition(new BABYLON.Vector3(0, 0, -20));
+  camera.setPosition(new BABYLON.Vector3(2, 4, 5));
+  camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
+  camera.orthoTop = 15;
+  camera.orthoBottom = -15;
+  camera.orthoLeft = -20;
+  camera.orthoRight = 20;
 
-  // const box = new BABYLON.MeshBuilder.CreateBox('myBox', {
-  //   size: 0.2,
-  //   width: .1,
-  //   height: .3,
-  //   depth: .1,
-  //   colors: [
-  //     new BABYLON.Color4(1, 0, 0, .4)
-  //   ]
-  // });
+  const box = new BABYLON.MeshBuilder.CreateBox('myBox', {
+    size : 1,
+    height: 3
+  }, scene);
 
-  // const sphere = new BABYLON.MeshBuilder.CreateSphere('mySphere', {
-  //   segments: 50,
-  //   diameter: 0.3,
-  //   diameterX: 0.1
-  // });
+  box.enableEdgesRendering();
+  box.edgesWidth = 1.0;
+  box.edgesColor = new BABYLON.Color4(0, 1, 1, 1);
+
+  const boxMaterial = new BABYLON.StandardMaterial();
+  box.material = boxMaterial;
+  boxMaterial.emissiveColor = new BABYLON.Color3(0, 1, 1);
+  boxMaterial.alpha = .1;
+
+  // boxMaterial.diffuseColor = new BABYLON.Color3(0, 1, 0);
+  // boxMaterial.specularColor = new BABYLON.Color3(1, 0, 0);
+
+  // boxMaterial.ambientColor = new BABYLON.Color3(0, 1, 1);
+  // scene.ambientColor = new BABYLON.Color3(0, 1, 1);
+
+  // boxMaterial.wireframe = true;
+
 
   // const ground = new BABYLON.MeshBuilder.CreateGround('', {
   //   height: 10,
@@ -45,11 +56,22 @@ const createScene = async function() {
   //   subdivisions: 30
   // });
 
+  // const light = new BABYLON.DirectionalLight(
+  //   'directionnalLight',
+  //   new BABYLON.Vector3(-2, -3, 0),
+  //   scene
+  // );
+  // light.intensity = .5;
+
   const fontData = await (await fetch('/Montserrat_Bold.json')).json();
-  const text = BABYLON.MeshBuilder.CreateText('', 'A', fontData, {
+  const text = BABYLON.MeshBuilder.CreateText('', 'P', fontData, {
     size: 1,
-    depth: .3
-  });
+    depth: .2
+  }, scene);
+
+  const textMaterial = new BABYLON.StandardMaterial();
+  text.material = textMaterial;
+  textMaterial.emissiveColor = new BABYLON.Color4(1, 1, 1, 1);
 
   return scene;
 }
