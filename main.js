8c1542eb42481c72bff445a287bc4963f43886af
Clément Menou <clementmenou0@gmail.com>
2023-10-07 21:04:40 +0200

    Babylon : "finished" now integrating in loltrainer

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 import * as BABYLON from 'babylonjs';
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 const canvas = document.getElementById('renderCanvas');
 
 const engine = new BABYLON.Engine(canvas);
@@ -7,71 +13,55 @@ const engine = new BABYLON.Engine(canvas);
 const createScene = async function() {
   const scene = new BABYLON.Scene(engine);
 
-  // scene.createDefaultCameraOrLight(true, false, true);
-
-  const camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene)
+  const camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 0, new BABYLON.Vector3(0, 0, 0), scene)
   
-  camera.setPosition(new BABYLON.Vector3(2, 4, 5));
-  camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
-  camera.orthoTop = 15;
-  camera.orthoBottom = -15;
-  camera.orthoLeft = -20;
-  camera.orthoRight = 20;
+  camera.setPosition(new BABYLON.Vector3(400, 800, -800));
+  camera.fov = .4;
 
-  const box = new BABYLON.MeshBuilder.CreateBox('myBox', {
-    size : 1,
-    height: 3
-  }, scene);
-
-  box.enableEdgesRendering();
-  box.edgesWidth = 1.0;
-  box.edgesColor = new BABYLON.Color4(0, 1, 1, 1);
+  camera.attachControl();
 
   const boxMaterial = new BABYLON.StandardMaterial();
-  box.material = boxMaterial;
-  boxMaterial.emissiveColor = new BABYLON.Color3(0, 1, 1);
-  boxMaterial.alpha = .1;
+  const fontData = await (await fetch('/Montserrat_SemiBold.json')).json();
 
-  // boxMaterial.diffuseColor = new BABYLON.Color3(0, 1, 0);
-  // boxMaterial.specularColor = new BABYLON.Color3(1, 0, 0);
+  for(let i = 0; i < 5; i++){
+    let box = new BABYLON.MeshBuilder.CreateBox('myBox', {
+      size: 16,
+      height: 40
+    }, scene);
+    box.enableEdgesRendering();
+    box.edgesWidth = 100;
+    box.edgesColor = new BABYLON.Color4(0, 1, 1, 1);
+    boxMaterial.alpha = 0;
+    box.material = boxMaterial;
+    let boxCoordX = getRandomInt(-250, 250);
+    let boxCoordZ = getRandomInt(-250, 250);
+    box.position = new BABYLON.Vector3(boxCoordX, 15, boxCoordZ);
 
-  // boxMaterial.ambientColor = new BABYLON.Color3(0, 1, 1);
-  // scene.ambientColor = new BABYLON.Color3(0, 1, 1);
+    const text1 = BABYLON.MeshBuilder.CreateText('', 'A', fontData, {
+      size: 12,
+      depth: 1
+    }, scene);
 
-  // boxMaterial.wireframe = true;
+    const textMaterial = new BABYLON.StandardMaterial();
+    text1.material = textMaterial;
+    textMaterial.emissiveColor = new BABYLON.Color4(1, 1, 1, 1);
 
+    text1.position = new BABYLON.Vector3(0, 17, 8);
+  }
 
-  // const ground = new BABYLON.MeshBuilder.CreateGround('', {
-  //   height: 10,
-  //   width: 10,
-  //   subdivisions: 1
-  // });
+  
 
-  // ground.material = new BABYLON.StandardMaterial();
-  // ground.material.wireframe = true;
+  
 
-  // const groundFromHM = new BABYLON.MeshBuilder.CreateGroundFromHeightMap('', /* link to black and white image */, {
-  //   height: 10,
-  //   width: 10,
-  //   subdivisions: 30
-  // });
+  const ground = new BABYLON.MeshBuilder.CreateGround('', {
+    height: 4000,
+    width: 4000
+  });
 
-  // const light = new BABYLON.DirectionalLight(
-  //   'directionnalLight',
-  //   new BABYLON.Vector3(-2, -3, 0),
-  //   scene
-  // );
-  // light.intensity = .5;
-
-  const fontData = await (await fetch('/Montserrat_Bold.json')).json();
-  const text = BABYLON.MeshBuilder.CreateText('', 'P', fontData, {
-    size: 1,
-    depth: .2
-  }, scene);
-
-  const textMaterial = new BABYLON.StandardMaterial();
-  text.material = textMaterial;
-  textMaterial.emissiveColor = new BABYLON.Color4(1, 1, 1, 1);
+  const groundMaterial = new BABYLON.StandardMaterial();
+  groundMaterial.emissiveColor = new BABYLON.Color3(.1, .1, .1);
+  groundMaterial.alpha = .9;
+  ground.material = groundMaterial;
 
   return scene;
 }
@@ -81,8 +71,3 @@ const scene = await createScene();
 engine.runRenderLoop(function () {
   scene.render();
 });
-
-window.addEventListener('resize', function() {
-  engine.resize();
-});
-
